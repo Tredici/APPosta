17977bcc8523d627d6949c8997ec00f7afb50c99
Tredici <marcolucio1999@gmail.com>
2020-12-07 12:02:40 +0100

    Versione iniziale funzionante. Il codice non è granché, molte cose sono state fatte in modo stupido, tante cose andrebbero messe apposto, ma funziona.

--- APPosta/Form1.Designer.vb
+++ APPosta/Form1.Designer.vb
@@ -22,10 +22,236 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.StatusBox = New System.Windows.Forms.TextBox()
+        Me.SendButton = New System.Windows.Forms.Button()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.Destinatari = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Mittente = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Oggetto = New System.Windows.Forms.TextBox()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Panel3)
+        Me.Panel1.Controls.Add(Me.RichTextBox1)
+        Me.Panel1.Location = New System.Drawing.Point(12, 12)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(776, 357)
+        Me.Panel1.TabIndex = 1
+        '
+        'Panel3
+        '
+        Me.Panel3.Location = New System.Drawing.Point(0, 0)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(776, 114)
+        Me.Panel3.TabIndex = 1
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(1, 120)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(775, 237)
+        Me.RichTextBox1.TabIndex = 0
+        Me.RichTextBox1.Text = ""
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.SplitContainer1)
+        Me.Panel2.Location = New System.Drawing.Point(13, 375)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(775, 63)
+        Me.Panel2.TabIndex = 2
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.TextBox1)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.Label4)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.StatusBox)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.SendButton)
+        Me.SplitContainer1.Size = New System.Drawing.Size(775, 63)
+        Me.SplitContainer1.SplitterDistance = 258
+        Me.SplitContainer1.TabIndex = 0
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(4, 35)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.PlaceholderText = "mail.server.it"
+        Me.TextBox1.Size = New System.Drawing.Size(251, 23)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(4, 4)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(65, 15)
+        Me.Label4.TabIndex = 0
+        Me.Label4.Text = "Mail Server"
+        '
+        'StatusBox
+        '
+        Me.StatusBox.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.StatusBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.StatusBox.Location = New System.Drawing.Point(3, 35)
+        Me.StatusBox.Name = "StatusBox"
+        Me.StatusBox.ReadOnly = True
+        Me.StatusBox.Size = New System.Drawing.Size(507, 23)
+        Me.StatusBox.TabIndex = 1
+        Me.StatusBox.TabStop = False
+        '
+        'SendButton
+        '
+        Me.SendButton.Location = New System.Drawing.Point(419, 4)
+        Me.SendButton.Name = "SendButton"
+        Me.SendButton.Size = New System.Drawing.Size(91, 25)
+        Me.SendButton.TabIndex = 0
+        Me.SendButton.Text = "Invia Mail"
+        Me.SendButton.UseVisualStyleBackColor = True
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 2
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 26.28866!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 73.71134!))
+        Me.TableLayoutPanel1.Controls.Add(Me.Destinatari, 1, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label1, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.Mittente, 1, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label2, 0, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label3, 0, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.Oggetto, 1, 2)
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(12, 12)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 3
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 43.93939!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 56.06061!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 44.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(776, 114)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'Destinatari
+        '
+        Me.Destinatari.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Destinatari.Location = New System.Drawing.Point(207, 33)
+        Me.Destinatari.Name = "Destinatari"
+        Me.Destinatari.PlaceholderText = "receriver@other.place.it"
+        Me.Destinatari.Size = New System.Drawing.Size(566, 23)
+        Me.Destinatari.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(3, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(52, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Mittente"
+        '
+        'Mittente
+        '
+        Me.Mittente.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Mittente.Location = New System.Drawing.Point(207, 3)
+        Me.Mittente.Name = "Mittente"
+        Me.Mittente.PlaceholderText = "sender@place.it"
+        Me.Mittente.Size = New System.Drawing.Size(566, 23)
+        Me.Mittente.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(3, 30)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(63, 15)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Destinatari"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(3, 69)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(51, 15)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Oggetto"
+        '
+        'Oggetto
+        '
+        Me.Oggetto.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Oggetto.Location = New System.Drawing.Point(207, 72)
+        Me.Oggetto.Name = "Oggetto"
+        Me.Oggetto.PlaceholderText = "What's the mail about?"
+        Me.Oggetto.Size = New System.Drawing.Size(566, 23)
+        Me.Oggetto.TabIndex = 6
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
-    End Sub
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.Text = "APPosta"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel2.ResumeLayout(False)
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel1.PerformLayout()
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        Me.SplitContainer1.Panel2.PerformLayout()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.TableLayoutPanel1.PerformLayout()
+        Me.ResumeLayout(False)
 
+    End Sub
+    Friend WithEvents SendButton As Button
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Destinatari As TextBox
+    Friend WithEvents Mittente As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents SplitContainer1 As SplitContainer
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Oggetto As TextBox
+    Friend WithEvents StatusBox As TextBox
 End Class
--- APPosta/Form1.vb
+++ APPosta/Form1.vb
@@ -1,3 +1,61 @@
 ﻿Public Class Form1
 
+    Private smtpServer As String
+    Private sender As String
+    Private receiver As String
+    Private obj As String
+    Private content As String
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles SendButton.Click
+        Console.WriteLine("Sendig mail to {0} from {1} at {2}", receiver, sender, smtpServer)
+        Dim mailClient = New System.Net.Mail.SmtpClient(Me.smtpServer)
+        'mailClient.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network
+        'mailClient.EnableSsl = True
+        'mailClient.Port = 993
+        mailClient.Timeout = 5000 ' 15 secondi timeout
+        Try
+            StatusBox.Text = "Sending email...."
+            Me.Refresh()
+            mailClient.Send(Me.sender, Me.receiver, Me.obj, Me.content)
+            StatusBox.Text = "Email successfully sent!"
+        Catch ex As Exception
+            StatusBox.Text = "Sending Timed Out!"
+        End Try
+        mailClient.Dispose()
+        Me.Refresh()
+    End Sub
+
+    Private Sub RichTextBox1_TextChanged(sender As Object, e As EventArgs) Handles RichTextBox1.TextChanged
+        Dim mailText = DirectCast(sender, RichTextBox)
+        Dim content As String = mailText.Text
+        Me.content = content
+    End Sub
+
+    Private Sub Mittente_TextChanged(sender As Object, e As EventArgs) Handles Mittente.TextChanged
+        Dim senderBox = DirectCast(sender, TextBox)
+        Me.sender = senderBox.Text
+    End Sub
+
+    Private Sub Destinatari_TextChanged(sender As Object, e As EventArgs) Handles Destinatari.TextChanged
+        Dim receiversBox = DirectCast(sender, TextBox)
+        Me.receiver = receiversBox.Text
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        Dim serverBox = DirectCast(sender, TextBox)
+        Me.smtpServer = serverBox.Text
+    End Sub
+
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs)
+
+    End Sub
+
+    Private Sub Oggetto_TextChanged(sender As Object, e As EventArgs) Handles Oggetto.TextChanged
+        Dim objectBox = DirectCast(sender, TextBox)
+        Me.obj = objectBox.Text
+    End Sub
 End Class
